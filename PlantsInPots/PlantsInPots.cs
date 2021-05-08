b65a4d8a9d419b6099c26a4d794cd2fad086b19a
Baruch Velez <baruchvelez@gmail.com>
2021-05-08 11:57:43 +0200

    init shared folder

--- PlantsInPots/PlantsInPots.cs
+++ PlantsInPots/PlantsInPots.cs
@@ -6,31 +6,25 @@
 
 using BepInEx;
 using UnityEngine;
-using BepInEx.Configuration;
-using Jotunn.Utils;
 using HarmonyLib;
+using PlantsInPots.Shared;
 
 namespace PlantsInPots
 {
   [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
   [BepInDependency(Jotunn.Main.ModGuid)]
-  //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
   internal class PlantsInPots : BaseUnityPlugin
   {
     public const string PluginGUID = "com.sveit.PlantsInPots";
     public const string PluginName = "PlantsInPots";
     public const string PluginVersion = "0.0.1";
-    private Harmony harmony = new Harmony(PluginGUID);
+    private readonly Harmony harmony = new Harmony(PluginGUID);
 
     private void Awake()
     {
-      // Do all your init stuff here
-      // Acceptable value ranges can be defined to allow configuration via a slider in the BepInEx ConfigurationManager: https://github.com/BepInEx/BepInEx.ConfigurationManager
-      // Config.Bind<int>("Main Section", "Example configuration integer", 1, new ConfigDescription("This is an example config, using a range limitation for ConfigurationManager", new AcceptableValueRange<int>(0, 100)));
-
-      // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
       Jotunn.Logger.LogInfo("PlantsInPots Loaded.");
       harmony.PatchAll();
+      RegisterPrefabs();
     }
 
     private void OnDestroy()
@@ -38,19 +32,16 @@ namespace PlantsInPots
       harmony.UnpatchAll();
     }
 
-    [HarmonyPatch(typeof(Character), nameof(Character.Jump))]
-    class CharacterJumpPatch
+    private void RegisterPrefabs()
     {
-      static void Postfix(ref float ___m_jumpForce)
-      {
-        ___m_jumpForce += 10;
-      }
+      // get prefab for pot? idk...s
+      AssetBundle bundle = AssetHelper.GetBundle(AssetNames.YellowPlant);
     }
 
 #if DEBUG
     private void Update()
     {
-      if (Input.GetKeyDown(KeyCode.F6))
+      if (Input.GetKeyDown(KeyCode.F4))
       { // Set a breakpoint here to break on F6 key press
         
       }
---
+++ PlantsInPots/Shared/AssetHelper.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Reflection;
+using System.IO;
+
+namespace PlantsInPots.Shared
+{
+  class AssetNames
+  {
+    private readonly static string Prefix = "PlantsInPots";
+    public static string YellowPlant { get { return $"{Prefix}_Yellow"; } }
+    public static string ThistlePlant {  get { return $"{Prefix}_Thistle"; } }
+    public static string SmallPot { get { return $"{Prefix}_SmallPot"; } }
+  }
+  class AssetHelper
+  {
+    public static AssetBundle GetBundle(string filename)
+    {
+      Assembly execAssembly = Assembly.GetExecutingAssembly();
+      string resourceName = execAssembly.GetManifestResourceNames().Single(str => str.EndsWith(filename));
+
+      AssetBundle bundle;
+
+      using (Stream stream = execAssembly.GetManifestResourceStream(resourceName))
+      {
+        bundle = AssetBundle.LoadFromStream(stream);
+      }
+
+      return bundle;
+    }
+  }
+}
